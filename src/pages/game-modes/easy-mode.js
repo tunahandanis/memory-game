2a2721ffaff9a17e522a2831f9ba7ed69c09ca99
Tunahan Danış <tunahandanis@outlook.com>
2022-04-07 16:04:42 +0300

    add card component

--- src/pages/game-modes/easy-mode.js
+++ src/pages/game-modes/easy-mode.js
@@ -1,5 +1,28 @@
+import Card from "../components/Card";
+
+const cardImages = [
+  { src: "/static/icons/bitcoin.png" },
+  { src: "/static/icons/ethereum.png" },
+  { src: "/static/icons/cardano.png" },
+  { src: "/static/icons/rose.png" },
+  { src: "/static/icons/xrp.png" },
+  { src: "/static/icons/bnb.png" },
+];
+
 const EasyMode = () => {
-  return <div></div>;
+  return (
+    <div>
+      <header>
+        <h2>TILE Points: </h2>
+      </header>
+
+      <div>
+        {cardImages.map((card) => (
+          <Card card={card} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default EasyMode;
